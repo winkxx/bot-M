9086f4b851b80a5cc814cfc067d7c7e0e55161e0
winkxx <68945988+winkxx@users.noreply.github.com>
2021-07-21 16:54:24 +0800

    Update web.py

--- bot/web.py
+++ bot/web.py
@@ -55,6 +55,6 @@ def index():
 
 if __name__ == '__main__':
 
-    #port = int(os.environ.get('PORT'))
-    port=6000
+    port = int(os.environ.get('PORT'))
+    #port=6000
     app.run(host='0.0.0.0', port=port, debug=True)
